cbaeebcf5d55b7faa58aed5c888a86dbaea9236a
limZyuan <58379837+limZyuan@users.noreply.github.com>
2021-07-09 14:07:11 +0800

    remove public path

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,6 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: "/",
     assetModuleFilename: "./assets/[name][ext]",
   },
   devServer: {
